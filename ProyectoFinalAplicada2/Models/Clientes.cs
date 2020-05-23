572e9826ffe33ab50955ad3f88ddf667067defed
Darwin-afk <darwin_lantigua@ucne.edu.do>
2020-05-23 16:05:36 -0400

    Creacion de los modelos: Clientes, Usuarios.

---
+++ ProyectoFinalAplicada2/Models/Clientes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoFinalAplicada2.Models
+{
+    public class Clientes
+    {
+        [Key]
+        public int ClienteId { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Nombres { get; set; }
+        public string Cedula { get; set; }
+        public string Telefono { get; set; }
+        public string Celular { get; set; }
+        public string Direccion { get; set; }
+        public string Email { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public DateTime FechaModificacion { get; set; }
+        public int UsuarioId { get; set; }
+    }
+}
---
+++ ProyectoFinalAplicada2/Models/Usuarios.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoFinalAplicada2.Models
+{
+    public class Usuarios
+    {
+        [Key]
+        public int UsuarioId { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Nombres { get; set; }
+        public string NombreUsuario { get; set; }
+        public string Clave { get; set; }
+        public string Email { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public DateTime FechaModificacion { get; set; }
+        public int UsuarioIdCreacion { get; set; }
+    }
+}
