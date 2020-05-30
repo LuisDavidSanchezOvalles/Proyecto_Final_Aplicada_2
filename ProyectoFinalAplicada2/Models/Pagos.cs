252c74eda6243e874b89446ec44642cfeef205a3
Darwin-afk <darwin_lantigua@ucne.edu.do>
2020-05-30 14:15:03 -0400

    Creacion de los modelos: Cacaos, Pagos, PagosDetalle.

---
+++ ProyectoFinalAplicada2/Models/Pagos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProyectoFinalAplicada2.Models
+{
+    public class Pagos
+    {
+        [Key]
+        public int PagoId { get; set; }
+        public DateTime Fecha { get; set; }
+        public int ClienteId { get; set; }
+        public decimal Total { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public DateTime FechaModificacion { get; set; }
+        public int UsuarioId { get; set; }
+
+        [ForeignKey("PagoId")]
+        public virtual List<PagosDetalle> PagoDetalle { get; set; }
+    }
+}
---
+++ ProyectoFinalAplicada2/Models/PagosDetalle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoFinalAplicada2.Models
+{
+    public class PagosDetalle
+    {
+        [Key]
+        public int PagoDetalleId { get; set; }
+        public int PagoId { get; set; }
+        public int VentaId { get; set; }
+        public decimal Monto { get; set; }
+        public decimal Saldo { get; set; }
+    }
+}
